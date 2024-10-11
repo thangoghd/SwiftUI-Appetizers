a3df6b87cd0b4ecfa4c499884de15818a783d458
thangoghd <thangteenx2@gmail.com>
2024-10-11 22:59:54 +0700

    Enhance CartView and EditAppetizerCartView functionality and UI

---
+++ SwiftUI-Appetizers/Pages/EditAppetizerCartView.swift
@@ -0,0 +1,18 @@
+//
+//  EditAppetizerCartView.swift
+//  SwiftUI-Appetizers
+//
+//  Created by Ha Viet Thang on 11/10/24.
+//
+
+import SwiftUI
+
+struct EditAppetizerCartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditAppetizerCartView()
+}
